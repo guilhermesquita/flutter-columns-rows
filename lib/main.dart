2c43c3af30a8997645c86d0c8d3d018df9265854
guilherme <guilhermer_mesquita@hotmail.com>
2024-02-09 11:59:17 -0400

    feat: create a new class for a login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:horas_v3/screens/login_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -15,31 +16,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'flutter demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-          ),
-          Container(
-            color: Colors.blue,
-            width: 100,
-            height: 100,
-          ),
-          Container(
-            color: Colors.green,
-            width: 100,
-            height: 100,
-          ),
-        ],
-      ),
+      home: const LoginScreen(),
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: const Center(
+          child: Text(
+        'teste',
+        style: TextStyle(color: Colors.white),
+      )),
+      // child: const Colum1n(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      // ),
+    );
+  }
+}
